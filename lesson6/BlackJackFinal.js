ad174832ae3f2b2bc8489b14946053b9a67e00a5
BobEverett <rjeverett2@gmail.com>
2020-10-24 09:31:13 -0700

    Updated Black Jack files

--- lesson6/BlackJackFinal.js
+++ lesson6/BlackJackFinal.js
@@ -1,80 +1,117 @@
-const fModules = require("./functionModules.js");
+/* eslint-disable no-console */
 const fs = require('fs');
 const colors = require('colors');
 const rlsync = require('readline-sync');
+const fModules = require('./functionModules.js');
+
 const fileData = fs.readFileSync('./blackJackMessages.json', 'utf8');
 const MESSAGES = JSON.parse(fileData);
 
-
 const CARDS = {
-  hearts : { '2H' : 2, '3H': 3, '4H' : 4, '5H' : 5, '6H' : 6, '7H' : 7, '8H' : 8,
-    '9H' : 9, '10H' : 10, 'JH' : 10, 'QH' : 10, 'KH' : 10, 'AH' : [11, 1]},
-  diamonds : { '2D' : 2, '3D': 3, '4D' : 4, '5D' : 5, '6D' : 6, '7D' : 7, '8D' : 8,
-    '9D' : 9, '10D' : 10, 'JD' : 10, 'QD' : 10, 'KD' : 10, 'AD' : [11, 1]},
-  clubs : { '2C' : 2, '3C': 3, '4C' : 4, '5C' : 5, '6C' : 6, '7C' : 7, '8C' : 8,
-    '9C' : 9, '10C' : 10, 'JC' : 10, 'QC' : 10, 'KD' : 10, 'AC' : [11, 1]},
-  spades : { '2S' : 2, '3S': 3, '4S' : 4, '5S' : 5, '6S' : 6, '7S' : 7, '8S' : 8,
-    '9S' : 9, '10S' : 10, 'JS' : 10, 'QS' : 10, 'KS' : 10, 'AS' : [11, 1]}
+  hearts: {
+    '2H': 2,
+    '3H': 3,
+    '4H': 4,
+    '5H': 5,
+    '6H': 6,
+    '7H': 7,
+    '8H': 8,
+    '9H': 9,
+    '10H': 10,
+    JH: 10,
+    QH: 10,
+    KH: 10,
+    AH: [11, 1],
+  },
+  diamonds: {
+    '2D': 2,
+    '3D': 3,
+    '4D': 4,
+    '5D': 5,
+    '6D': 6,
+    '7D': 7,
+    '8D': 8,
+    '9D': 9,
+    '10D': 10,
+    JD: 10,
+    QD: 10,
+    KD: 10,
+    AD: [11, 1],
+  },
+  clubs: {
+    '2C': 2,
+    '3C': 3,
+    '4C': 4,
+    '5C': 5,
+    '6C': 6,
+    '7C': 7,
+    '8C': 8,
+    '9C': 9,
+    '10C': 10,
+    JC: 10,
+    QC: 10,
+    KD: 10,
+    AC: [11, 1],
+  },
+  spades: {
+    '2S': 2,
+    '3S': 3,
+    '4S': 4,
+    '5S': 5,
+    '6S': 6,
+    '7S': 7,
+    '8S': 8,
+    '9S': 9,
+    '10S': 10,
+    JS: 10,
+    QS: 10,
+    KS: 10,
+    AS: [11, 1],
+  },
 };
 
-let personHand = [];
-let dealerHand = [];
+let personHand;
+let dealerHand;
 let deck;
 
 function shuffleCards() {
-  deck = Object.keys(CARDS).reduce((combine,elem) => {
-    return combine.concat(Object.keys(CARDS[elem]));
-  }, []);
+  deck = Object.keys(CARDS).reduce((combine, elem) => combine.concat(Object.keys(CARDS[elem])), []);
 
-  for (let cards = deck.length; cards > 0; cards--) {
-    let randNum = Math.floor(Math.random() * cards);
+  for (let cards = deck.length; cards > 0; cards -= 1) {
+    const randNum = Math.floor(Math.random() * cards);
     deck.push(deck.splice(randNum, 1)[0]);
   }
   return deck;
 }
 
-deck = shuffleCards();
-
-function dealHand(number, deck) {
-  for (let numCards = number; numCards > 0; numCards--) {
+function dealHand(number) {
+  for (let numCards = number; numCards > 0; numCards -= 1) {
     personHand.push(deck.shift());
     dealerHand.push(deck.shift());
   }
 }
 
-function displayCards(number) {
-  if (number === 1) {
-    console.log(`\nYour Cards: ${personHand.join(' | ')}` +
-      ` | Hand = ${countHandValue(personHand)}\n`);
-    console.log(colors.green.bold(`Dealer Cards:  ${dealerHand[0]} | ?\n`));
-  } else {
-    console.log(`\nYour Cards: ${personHand.join(' | ')}` +
-      ` | Hand = ${countHandValue(personHand)}\n`);
-    console.log(colors.green.bold(`Dealer Cards: ${dealerHand.join(' | ')}` +
-      ` | Hand = ${countHandValue(dealerHand)}\n`));
-  }
-}
-
 function findCardValue(card) {
   let cardValue;
-  for (const suit of Object.keys(CARDS)) {
-    if (Object.keys(CARDS[suit]).includes(card)) {
-      cardValue = CARDS[suit][card];
-    }
+  if (['J', 'Q', 'K'].includes(card[0]) || card.slice(0, 2) === '10') {
+    cardValue = 10;
+  } else if (card[0] === 'A') {
+    cardValue = [1, 11];
+  } else {
+    cardValue = Number(card[0]);
   }
   return cardValue;
 }
 
 function isAce(card) {
-  return Array.isArray(findCardValue(card));
+  return card[0] === 'A';
 }
 
 function countAceHandValue(hand) {
-  let tempValue = (hand.filter(card => !isAce(card))).reduce((sum, card) => {
-    return sum + findCardValue(card);
-  }, 0);
-  let numAces = hand.filter(card => (isAce(card)));
-  for (let num = numAces.length; num > 0; num--) {
+  let tempValue = (hand.filter((card) => !isAce(card)))
+    .reduce((sum, card) => sum + findCardValue(card), 0);
+  const numAces = hand.filter((card) => (isAce(card)));
+  for (let num = numAces.length; num > 0; num -= 1) {
     if (tempValue > 10) {
       tempValue += 1;
     } else {
@@ -85,49 +122,71 @@ function countAceHandValue(hand) {
 }
 
 function countHandValue(hand) {
-  if (hand.some(card => isAce(card))) {
+  if (hand.some((card) => isAce(card))) {
     return countAceHandValue(hand);
-  } else {
-    let handValue = 0;
-    for (let card of hand) {
-      handValue += findCardValue(card);
-    }
-    return (handValue);
   }
+  const handValue = hand.reduce((sum, card) => sum + findCardValue(card), 0);
+  return (handValue);
+}
+
+function displayCards(number) {
+  if (number === 1) {
+    console.log(`\nYour Cards: ${personHand.join(' | ')}`
+      + ` | Hand = ${countHandValue(personHand)}\n`);
+    console.log(colors.green.bold(`Dealer Cards:  ${dealerHand[0]} | ?\n`));
+  } else {
+    console.log(`\nYour Cards: ${personHand.join(' | ')}`
+      + ` | Hand = ${countHandValue(personHand)}\n`);
+    console.log(colors.green.bold(`Dealer Cards: ${dealerHand.join(' | ')}`
+      + ` | Hand = ${countHandValue(dealerHand)}\n`));
+  }
+}
+
+function pauseGame(reason) {
+  let playerInput;
+  do {
+    if (reason === 'nextCard') {
+      playerInput = rlsync.question(fModules.prompt(colors.brightBlue.bold(MESSAGES.nextCard)));
+    } else {
+      playerInput = rlsync.question(fModules.prompt(colors.brightBlue.bold(MESSAGES.newHand)));
+    }
+    if (playerInput.toLowerCase() !== 'n') {
+      fModules.prompt(colors.brightBlue.bold(MESSAGES.invalidEntry));
+    }
+  } while (playerInput.toLowerCase() !== 'n');
+  console.clear();
 }
 
 function hitOrStay() {
   let playerInput;
   do {
-    playerInput = rlsync.question(fModules.prompt(colors.bold.italic(MESSAGES['userInput'])));
+    playerInput = rlsync.question(fModules.prompt(colors.bold.italic(MESSAGES.userInput)));
     if ((!['h', 's'].includes(playerInput.toLowerCase()))) {
-      fModules.prompt(colors.red.italic(MESSAGES['invalidInput']));
+      fModules.prompt(colors.red.italic(MESSAGES.invalidInput));
     }
   } while (!['h', 's'].includes(playerInput.toLowerCase()));
 
-  if (playerInput.toLowerCase() === 's' ) {
+  if (playerInput.toLowerCase() === 's') {
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
-function newCard(player, deck) {
+function newCard(player) {
   if (player === 'person') {
     return personHand.push(deck.shift());
-  } else {
-    pauseGame();
-    return dealerHand.push(deck.shift());
   }
+  pauseGame('nextCard');
+  return dealerHand.push(deck.shift());
 }
 
 function isBusted(hand) {
   return countHandValue(hand) > 21;
 }
 
-function determineHandWinner(personHand, dealerHand) {
-  let playerBusted = isBusted(personHand);
-  let dealerBusted = isBusted(dealerHand);
+function determineHandWinner() {
+  const playerBusted = isBusted(personHand);
+  const dealerBusted = isBusted(dealerHand);
 
   if (playerBusted) return 'dealer';
   if (dealerBusted) return 'person';
@@ -143,28 +202,17 @@ function determineHandWinner(personHand, dealerHand) {
   return 'draw';
 }
 
-function displayHandResult(personHand, dealerHand) {
-  let result = determineHandWinner(personHand, dealerHand);
+function displayHandResult() {
+  const result = determineHandWinner(personHand, dealerHand);
 
   if (result === 'person') {
-    fModules.prompt(colors.brightYellow.bold.italic(MESSAGES['youWonHand']));
+    fModules.prompt(colors.brightYellow.bold.italic(MESSAGES.youWonHand));
   } else if (result === 'dealer') {
-    fModules.prompt(colors.brightYellow.bold.italic(MESSAGES['dealerWonHand']));
+    fModules.prompt(colors.brightYellow.bold.italic(MESSAGES.dealerWonHand));
   } else {
-    fModules.prompt(colors.brightYellow.bold.italic(MESSAGES['draw']));
+    fModules.prompt(colors.brightYellow.bold.italic(MESSAGES.draw));
   }
   return result;
-}
-
-function pauseGame() {
-  let playerInput;
-  do {
-    playerInput = rlsync.question(fModules.prompt(colors.brightBlue.bold(MESSAGES['nextPlay'])));
-    if (playerInput.toLowerCase() !== 'n') {
-      fModules.prompt(colors.brightBlue.bold(MESSAGES['invalidEntry']));
-    }
-  } while (playerInput.toLowerCase() !== 'n');
-  console.clear();
 }
 
 function checkForMatchWinner(playerHandsWon, dealerHandsWon) {
@@ -176,26 +224,25 @@ function checkForMatchWinner(playerHandsWon, dealerHandsWon) {
 
 function displayMatchScore(playerHandsWon, dealerHandsWon) {
   if (checkForMatchWinner(playerHandsWon, dealerHandsWon)) {
-    fModules.prompt(colors.bold(MESSAGES['currentScore'] +
-       ` You = ${playerHandsWon} | Dealer = ${dealerHandsWon}\n`));
-    pauseGame();
+    fModules.prompt(colors.bold(`${MESSAGES.currentScore
+    } You = ${playerHandsWon} | Dealer = ${dealerHandsWon}\n`));
+    pauseGame('nextHand');
   } else {
-
     console.log('$'.repeat(80));
-    fModules.prompt(colors.red.bold(MESSAGES['finalScore'] +
-       ` You = ${playerHandsWon} | Dealer = ${dealerHandsWon}\n`));
+    fModules.prompt(colors.red.bold(`${MESSAGES.finalScore
+    } You = ${playerHandsWon} | Dealer = ${dealerHandsWon}\n`));
     if (playerHandsWon > dealerHandsWon) {
-      fModules.prompt(MESSAGES['youWonMatch']);
+      fModules.prompt(MESSAGES.youWonMatch);
     } else {
-      fModules.prompt(MESSAGES['dealerWonMatch']);
+      fModules.prompt(MESSAGES.dealerWonMatch);
     }
-    console.log('$'.repeat(80) + '\n');
+    console.log(`${'$'.repeat(80)}\n`);
   }
 }
 
 // Game Play
 {
-  fModules.prompt(colors.bold(MESSAGES['greeting']));
+  fModules.prompt(colors.bold(MESSAGES.greeting));
   let playerHandsWon = 0;
   let dealerHandsWon = 0;
 
@@ -223,7 +270,7 @@ function displayMatchScore(playerHandsWon, dealerHandsWon) {
       }
     }
 
-    let result = displayHandResult(personHand, dealerHand);
+    const result = displayHandResult(personHand, dealerHand);
 
     if (result === 'person') {
       playerHandsWon += 1;
@@ -232,6 +279,5 @@ function displayMatchScore(playerHandsWon, dealerHandsWon) {
     }
 
     displayMatchScore(playerHandsWon, dealerHandsWon);
-
   } while (checkForMatchWinner(playerHandsWon, dealerHandsWon));
 }
